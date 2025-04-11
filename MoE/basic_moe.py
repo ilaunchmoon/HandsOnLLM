a6748189125a857a556960e2daed8191e6c0efd8
ilaunchmoon <imoon_8295@163.com>
2025-04-11 23:23:32 +0800

    添加Moe最原始的雏形

--- MoE/basic_moe.py
+++ MoE/basic_moe.py
@@ -1,3 +1,21 @@
 import torch
 import torch.nn as nn 
 
+class BasicMoe(nn.Module):
+    def __init__(self, feat_in:int, hidden_dim:int, feat_out:int)->None:
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(feat_in, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, feat_out)
+        )
+
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        return self.net(x)
+    
+
+
+
