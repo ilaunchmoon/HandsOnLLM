95960710ab351c6ef67a016f5d705d8ee466303e
ilaunchmoon <imoon_8295@163.com>
2025-04-28 00:36:22 +0800

    添加llama3的FFN模块

---
+++ FeedForward/llama3_ffn.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from dataclasses import dataclass
+
+
+@dataclass
+class LlamaConfig:
+    hidden_dim: int          # 隐藏层维度
+    intermediate_dim: int    # 中间层维度
+    dropout_rate: float      # dropout 比率
+    bias: bool = True        # 是否使用偏置
+
+
+class LlamaFNN(nn.Module):
+    def __init__(self, config:LlamaConfig)->None:
+        super().__init__()
+        pass
+
+
+    def forward(self, x:torch.Tensor)->torch.Tensor:
+        pass
+
+    
